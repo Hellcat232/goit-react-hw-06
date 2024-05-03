2750574b076761c4f0e1b61bdabb438b8d759271
Hellcat232 <maxkuich1993@gmail.com>
2024-05-04 02:11:46 +0300

    hhjh

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,12 @@ import SearchBox from "../SearchBox/SearchBox.jsx";
 import ContactList from "../ContactList/ContactList.jsx";
 import { useEffect, useState } from "react";
 
-const base = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+// const base = [
+//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+// ];
 
 const App = () => {
   const [states, setState] = useState(() => {
